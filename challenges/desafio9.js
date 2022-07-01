8e4477635b3bb8f98f796bec30ae984cfea32099
Italo Sergio <italosergio@graduate.org>
2022-07-01 02:28:14 -0300

    pass: challenge 9

---
+++ challenges/desafio9.js
@@ -0,0 +1,15 @@
+db.produtos
+  .find(
+    {
+      valoresNutricionais: {
+        $elemMatch: {
+          unidadeMedida: "kcal",
+          quantidade: { $lt: 500 },
+        },
+      },
+    },
+    {
+      _id: 0,
+      nome: 1,
+    },
+  );
